374f4c7a279bb27d008895a3ec916430125512f6
olsedim <olsedim@gmail.com>
2017-01-15 21:22:55 +0200

    Add files via upload

---
+++ server-01.js
@@ -0,0 +1,12 @@
+﻿function User(name) {
+	this.name = name;
+}
+
+User.prototype.hello = function(who) {
+	console.log("Hello," + who.name);
+}
+
+var igor = new User("Игорь");
+var brad = new User("Брад");
+
+brad.hello(igor);
